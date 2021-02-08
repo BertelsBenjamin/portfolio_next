98875fc29b76103feb1ce4390446d0797161deb2
Benjamin Bertels <bertels123@gmail.com>
2021-02-08 22:45:15 +0100

    strapi setup, baboos.js, patrimonium.js

---
+++ pages/work/baboos.js
@@ -0,0 +1,30 @@
+import { connectToDatabase } from '../../util/mongodb';
+import { useSelector } from 'react';
+import showdown from 'showdown';
+import parse from 'html-react-parser';
+import styles from '../../scss/Project.module.scss';
+
+const Baboos = ({ project }) => {
+  return (
+    <div className={styles.container}>
+      <h1>{project.title}</h1>
+      {parse(project.description_full)}
+    </div>
+  );
+};
+
+export async function getServerSideProps() {
+  const converter = new showdown.Converter();
+
+  const { db } = await connectToDatabase();
+
+  let project = await db.collection('projects').find({ title: "Baboo's" }).toArray();
+
+  project[0].description_full = converter.makeHtml(project[0].description_full);
+
+  return {
+    props: { project: JSON.parse(JSON.stringify(project))[0] },
+  };
+}
+
+export default Baboos;
---
+++ pages/work/proshop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Proshop = () => {
+  return (
+    <div>
+      <h1>Proshop</h1>
+    </div>
+  );
+};
+
+export default Proshop;
