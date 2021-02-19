a62309f88c7d91060e307064d3f5077b5d82a1ce
Benjamin Bertels <bertels123@gmail.com>
2021-02-19 22:45:18 +0100

    portfolio.js setup and some minor styling in Project.module.scss

--- pages/work/patrimonium.js
+++ pages/work/patrimonium.js
@@ -4,25 +4,37 @@ import showdown from 'showdown';
 import parse from 'html-react-parser';
 import styles from '../../scss/Project.module.scss';
 
-const Baboos = ({ project }) => {
+const Patrimonium = ({ project }) => {
   return (
     <div className={styles.container}>
       <div className={styles.heading}>
         <h1 className={styles.title}>{project.title}</h1>
         <div className={styles.actions}>
-          <a href={project.github_uri} target='_blank'>
-          <object
-            type='image/svg+xml'
-            data='../github-alt.svg'
-          >
-          </object>
-        </a>
-        {project.demo_uri ? <a href={project.demo_uri} target='_blank'>
-          <i className="mi-computer"></i>
-        </a> : null}
+          <div className={styles.action}>
+            <a href={project.github_uri} target='_blank'>
+              <object type='image/svg+xml' data='../github-alt.svg'></object>
+            </a>
+          </div>
+          {project.demo_uri ? (
+            <div className={styles.action}>
+              <a href={project.demo_uri} target='_blank'>
+                <i className='mi-computer'></i>
+              </a>
+            </div>
+          ) : (
+            <div>
+              <a
+                href={`mailto:${process.env.EMAIL}?subject=${project.title}&body=Hi Benjamin,%0D%0A%0D%0AI would like to know more about your project "Patrimonium".%0D%0A%0D%0ACould you please contact me for a demo?%0D%0A%0D%0AKindest regards,%0D%0A%0D%0A`}
+              >
+                <i className='mi-email accent'></i>
+              </a>
+            </div>
+          )}
         </div>
       </div>
-      {parse(project.description_full)}
+      <p className={styles.description_full}>
+        {parse(project.description_full)}
+      </p>
     </div>
   );
 };
@@ -32,7 +44,10 @@ export async function getServerSideProps() {
 
   const { db } = await connectToDatabase();
 
-  let project = await db.collection('projects').find({ title: 'Patrimonium' }).toArray();
+  let project = await db
+    .collection('projects')
+    .find({ title: 'Patrimonium' })
+    .toArray();
 
   project[0].description_full = converter.makeHtml(project[0].description_full);
 
@@ -41,4 +56,4 @@ export async function getServerSideProps() {
   };
 }
 
-export default Baboos;
+export default Patrimonium;
--- pages/work/portfolio.js
+++ pages/work/portfolio.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import { connectToDatabase } from '../../util/mongodb';
+import { useSelector } from 'react';
+import showdown from 'showdown';
+import parse from 'html-react-parser';
+import styles from '../../scss/Project.module.scss';
 
-const Portfolio = () => {
+const Portfolio = ({ project }) => {
   return (
-    <div>
-      <h1>Portfolio</h1>
+    <div className={styles.container}>
+      <div className={styles.heading}>
+        <h1 className={styles.title}>{project.title}</h1>
+        <div className={styles.actions}>
+          <div className={styles.action}>
+            <a href={project.github_uri} target='_blank'>
+              <object type='image/svg+xml' data='../github-alt.svg'></object>
+            </a>
+          </div>
+          {project.demo_uri ? (
+            <div className={styles.action}>
+              <a href={project.demo_uri} target='_blank'>
+                <i className='mi-computer'></i>
+              </a>
+            </div>
+          ) : (
+            <div>
+              <a
+                href={`mailto:${process.env.EMAIL}?subject=${project.title}&body=Hi Benjamin,%0D%0A%0D%0AI would like to know more about your project "Patrimonium".%0D%0A%0D%0ACould you please contact me for a demo?%0D%0A%0D%0AKindest regards,%0D%0A%0D%0A`}
+              >
+                <i className='mi-email accent'></i>
+              </a>
+            </div>
+          )}
+        </div>
+      </div>
+      <p className={styles.description_full}>
+        {parse(project.description_full)}
+      </p>
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  const converter = new showdown.Converter();
+
+  const { db } = await connectToDatabase();
+
+  let project = await db
+    .collection('projects')
+    .find({ title: 'Portfolio' })
+    .toArray();
+
+  project[0].description_full = converter.makeHtml(project[0].description_full);
+
+  return {
+    props: { project: JSON.parse(JSON.stringify(project))[0] },
+  };
+}
 
 export default Portfolio;
